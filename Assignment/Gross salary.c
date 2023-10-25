78526eb73ae901abe5796a16ba748af570dcb48c
yeshamodi12 <148933660+yeshamodi12@users.noreply.github.com>
2023-10-25 13:15:17 +0530

    commit

---
+++ Assignment/Gross salary.c
@@ -0,0 +1,15 @@
+//Gross Salary Program in C Programs 
+//Gross Salary = Basic Pay + DA + HR
+#include<stdio.h>
+int main(){
+	int basicpay,DA,Hr,gross;
+	printf("\n enter the value of basicpay :");
+	scanf("%d",&basicpay);
+	printf("\n enter the value of DA :");
+	scanf("%f",&r);
+	printf("\n enter the value of t :");
+	scanf("%f",&t);
+	intrest = p*r*t/100;
+	printf("\n simple intrest is: %.2f",intrest);
+	
+} 
